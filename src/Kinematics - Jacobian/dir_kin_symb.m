7662ac67acbe62ddc09b05958ffd74faacfd180b
MikFerrari <m.ferrari092@studenti.unibs.it>
2021-08-16 11:21:58 +0200

    symbolic computation of jacobian and its derivative + kinematic loop

--- src/Kinematics - Jacobian/dir_kin_symb.m
+++ src/Kinematics - Jacobian/dir_kin_symb.m
@@ -1,7 +1,36 @@
-function [outputArg1,outputArg2] = dir_kin_symb(inputArg1,inputArg2)
-%DIR_KIN_SYMB Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [S_gripper_symb,S_complete_symb] = dir_kin_symb
+
+    syms l1a l1b l1c l2 l3 tilt_angle ...
+         x0 y0 z0       x1a y1a z1a     x1b y1b z1b ...
+         x1c y1c z1c    x2 y2 z2        x3 y3 z3 ...
+         q1_sym(t) q2_sym(t) q3_sym(t)
+
+    x0 = 0;
+    y0 = 0;
+    z0 = 0;
+
+    x1a = x0;
+    y1a = y0;
+    z1a = z0+l1a;
+
+    x1b = x1a+l1b*cos(q1_sym);
+    y1b = x1a+l1b*sin(q1_sym);
+    z1b = z1a;
+
+    x1c = x1b+l1c*cos(tilt_angle)*sin(q1_sym);
+    y1c = y1b-l1c*cos(tilt_angle)*cos(q1_sym);
+    z1c = z1b+l1c*sin(tilt_angle);
+
+    x2 = x1c+(l2+q2_sym)*cos(tilt_angle)*sin(q1_sym);
+    y2 = y1c-(l2+q2_sym)*cos(tilt_angle)*cos(q1_sym);
+    z2 = z1c+(l2+q2_sym)*sin(tilt_angle);
+
+    x3 = x2+l3*cos(tilt_angle+q3_sym)*sin(q1_sym);
+    y3 = y2-l3*cos(tilt_angle+q3_sym)*cos(q1_sym);
+    z3 = z2+l3*sin(tilt_angle+q3_sym);
+    
+    S_gripper_symb = [x3; y3; z3];
+    S_complete_symb = [x0; y0; z0; x1a; y1a; z1a; x1b; y1b; z1b; x1c; y1c; z1c; x2; y2; z2; x3; y3; z3];
+     
 end
 
---
+++ src/WS & Robot Plotting/load_robot_data.m
@@ -0,0 +1,7 @@
+L = [2 1.1 3 1 1.5];
+% l1a = L(1); l1b = L(2); l1c = L(3); l2 = L(4); l3 = L(5);
+
+angle = pi/6;
+limits = [-pi/4 pi/2 0 3 -0.5*pi 0.5*pi];
+
+nLinks = 3;
